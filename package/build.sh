3855cbde0c9ed1a39fb0b5493ba18a2b20a59184
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 13:26:41 +0100

    use gfortran gcc 4.4

--- package/build.sh
+++ package/build.sh
@@ -5,11 +5,11 @@ mkdir -p "$DIR/bin/"
 printenv
 
 cd ACEMD
-make TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib"
+make TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" CC=gcc-4.4
 cp libplumed1plugin.so libplumed1plugin-rex.so "$DIR/lib"
 
 cd ../utilities/sum_hills
-make
+make FC=gfortran-4.4
 cp sum_hills "$DIR/bin"
 
 
