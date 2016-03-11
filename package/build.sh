d3d1f4badd50e34222f88a6cd851a9042a7f2a97
M J Harvey <m.j.harvey@acellera.com>
2016-03-11 13:40:31 +0100

    rev build

--- package/build.sh
+++ package/build.sh
@@ -1,3 +1,17 @@
+CC=gcc-4.4
+FC=gfortran-4.4
+
+which $CC
+if [ "$?" != "0" ]; then
+  CC=gcc
+fi
+which $FC
+if [ "$?" != "0" ]; then
+  FC=gfortran
+fi
+
+
+
 DIR="$CONDA_DEFAULT_ENV"
 mkdir -p "$DIR/lib/"
 mkdir -p "$DIR/bin/"
@@ -5,11 +19,11 @@ mkdir -p "$DIR/bin/"
 printenv
 
 cd ACEMD
-make TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" CC=gcc-4.4
+make TCL="-I$SYS_PREFIX/include -L$SYS_PREFIX/lib" CC=$CC
 cp libplumed1plugin.so libplumed1plugin-rex.so "$DIR/lib"
 
 cd ../utilities/sum_hills
-make FC=gfortran-4.4
+make FC=$FC
 cp sum_hills "$DIR/bin"
 
 
